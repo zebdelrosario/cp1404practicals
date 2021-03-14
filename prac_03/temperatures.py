dcdfe9ea90d094e0b82aa7e0303b67177c62f2b6
zebdelrosario <delrosariozeb@gmail.com>
2021-03-14 17:48:13 +1000

    Refactor temperatures program to use functions, tidy comments.

--- prac_03/temperatures.py
+++ prac_03/temperatures.py
@@ -6,21 +6,35 @@ Pseudocode for temperature conversion
 MENU = """C - Convert Celsius to Fahrenheit
 F - Convert Fahrenheit to Celsius
 Q - Quit"""
-print(MENU)
-choice = input(">>> ").upper()
-while choice != "Q":
-    if choice == "C":
-        celsius = float(input("Celsius: "))
-        fahrenheit = celsius * 9.0 / 5 + 32
-        print("Result: {:.2f} F".format(fahrenheit))
-    elif choice == "F":
-        # Hint: celsius = 5 / 9 * (fahrenheit - 32)
-        # Remove the "pass" statement when you are done. It's a placeholder.
-        fahrenheit = float(input("Fahrenheit: "))
-        celsius = (fahrenheit - 32) * 5 / 9
-        print("Result: {:.2f} C".format(celsius))
-    else:
-        print("Invalid option")
+
+
+def main():
     print(MENU)
     choice = input(">>> ").upper()
-print("Thank you.")
+    while choice != "Q":
+        if choice == "C":
+            convert_celsius_to_fahrenheit()
+        elif choice == "F":
+            convert_fahrenheit_to_celsius()
+        else:
+            print("Invalid option")
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Thank you.")
+
+
+def convert_celsius_to_fahrenheit():
+    """Converts user input in Celsius to Fahrenheit."""
+    celsius = float(input("Celsius: "))
+    fahrenheit = celsius * 9.0 / 5 + 32
+    print("Result: {:.2f} F".format(fahrenheit))
+
+
+def convert_fahrenheit_to_celsius():
+    """Converts user input in Fahrenheit to Celsius."""
+    fahrenheit = float(input("Fahrenheit: "))
+    celsius = (fahrenheit - 32) * 5 / 9
+    print("Result: {:.2f} C".format(celsius))
+
+
+main()
