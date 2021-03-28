4c3319b39a13c464765e222ba6244ad244b9567c
zebdelrosario <delrosariozeb@gmail.com>
2021-03-28 17:55:28 +1000

    Add list sorting to count occurences function, add display dictionary function

--- prac_05/word_occurences.py
+++ prac_05/word_occurences.py
@@ -3,18 +3,26 @@
 
 def main():
     # sentence = input("Text: ")
-    sentence = "this is the sentence provided the sentence is this"
+    sentence = "this is a collection of words of nice words this is a fun thing it is"
     words_occurrences_dict = count_occurrences(sentence)
+    display_dictionary(words_occurrences_dict)
 
 
 def count_occurrences(sentence):
     """Count how often a word occurs in the given sentence."""
     words = sentence.split(" ")
+    words.sort()
     words_occurrences_dict = {}
     for word in words:
         if word not in words_occurrences_dict.keys():
             words_occurrences_dict[word] = words.count(word)
     return words_occurrences_dict
+
+
+def display_dictionary(dictionary):
+    """Display a dictionary in aligned fashion."""
+    for key, value in dictionary.items():
+        print(f"{key} : {value}")
 
 
 main()
