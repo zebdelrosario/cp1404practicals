9a6e0f040246d05f67e231d2f858e1ecfa71882d
zebdelrosario <delrosariozeb@gmail.com>
2021-03-28 16:42:31 +1000

    Add print statement to neatly display dictionary, reformat constant

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -4,15 +4,17 @@ State names in a dictionary
 File needs reformatting
 """
 
-# TODO: Reformat this file so the dictionary code follows PEP 8 convention
-CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
-                "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
-print(CODE_TO_NAME)
+STATE_NAMES = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
+               "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
+print(STATE_NAMES)
 
 state_code = input("Enter short state: ").upper()
 while state_code != "":
-    if state_code in CODE_TO_NAME:
-        print(state_code, "is", CODE_TO_NAME[state_code])
+    if state_code in STATE_NAMES:
+        print(state_code, "is", STATE_NAMES[state_code])
     else:
         print("Invalid short state")
     state_code = input("Enter short state: ").upper()
+
+for code, name in STATE_NAMES.items():
+    print(f"{code:<3} is {name}")
