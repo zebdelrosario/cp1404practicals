289b200d57d191919ab875d1df6cde4788a90ef4
zebdelrosario <delrosariozeb@gmail.com>
2021-04-15 15:09:46 +1000

    Add languages to ProgrammingLanguage class

---
+++ prac_06/languages.py
@@ -0,0 +1,12 @@
+"""CP1404 Practical 06: Classes - Intermediate exercises."""
+from programming_language import ProgrammingLanguage
+
+
+def main():
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+
+
+
+main()
