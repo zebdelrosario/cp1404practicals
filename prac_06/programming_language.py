18645e954a6f7c1401233ad899968fff0badcbe0
zebdelrosario <delrosariozeb@gmail.com>
2021-04-15 15:11:44 +1000

    Add ProgrammingLanguage class, add __init__ method, add __str__ method

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -1,1 +1,13 @@
 """CP1404 Practical 06: Classes - Intermediate exercises."""
+
+
+class ProgrammingLanguage:
+    def __init__(self, language="", typing="", reflection="", year=0):
+        self.language = language
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def __str__(self):
+        return f"{self.language}, {self.typing}, {self.reflection}, First appeared in {self.year}"
+
