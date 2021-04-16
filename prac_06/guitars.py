aaa8d0c78d9c8c13ca3b2a3f182c831be60d5523
zebdelrosario <delrosariozeb@gmail.com>
2021-04-16 16:54:05 +1000

    Add get_valid_float function, refactor get_valid_number function

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -7,8 +7,8 @@ def main():
     """Store all of input guitars in a list, then print their details."""
     print("My guitars!")
     guitar_name = get_valid_string("Name: ")
-    guitar_year = get_valid_number("Year: ")
-    guitar_cost = float(get_valid_number("Cost: "))
+    guitar_year = get_valid_integer("Year: ")
+    guitar_cost = get_valid_float("Cost: ")
 
 
 def get_valid_string(prompt):
@@ -22,18 +22,32 @@ def get_valid_string(prompt):
             print("Value Error, please enter a STRING!")
 
 
-def get_valid_number(prompt):
-    """Prompt user until a valid number is entered."""
+def get_valid_float(prompt):
+    """Prompt user until a valid float is entered."""
+    is_valid_number = False
+    while not is_valid_number:
+        try:
+            number = float(input(prompt))
+            if number < MINIMUM_VALUE:
+                print(f"Number is too small! Enter a float > {MINIMUM_VALUE}!")
+            else:
+                return number
+        except ValueError:
+            print(f"Value Error; enter a valid float that's >{MINIMUM_VALUE}!")
+
+
+def get_valid_integer(prompt):
+    """Prompt user until a valid integer is entered."""
     is_valid_number = False
     while not is_valid_number:
         try:
             number = int(input(prompt))
             if number < MINIMUM_VALUE:
-                print(f"Number is too small! Enter a number > {MINIMUM_VALUE}!")
+                print(f"Number is too small! Enter an integer > {MINIMUM_VALUE}!")
             else:
                 return number
         except ValueError:
-            print(f"Value Error; enter a valid number that's >{MINIMUM_VALUE}!")
+            print(f"Value Error; enter a valid integer that's >{MINIMUM_VALUE}!")
 
 
 main()
