ef39c52a0e8a4d6c854f46df4b1f20f3d75841e8
zebdelrosario <delrosariozeb@gmail.com>
2021-04-16 16:40:12 +1000

    Add get_valid_number function

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -8,5 +8,18 @@ def main():
     print("My guitars!")
 
 
+def get_valid_number(prompt):
+    """Prompt user until a valid number is entered."""
+    is_valid_number = False
+    while not is_valid_number:
+        try:
+            number = int(input(prompt))
+            if number < MINIMUM_VALUE:
+                print(f"Number is too small! Enter a number > {MINIMUM_VALUE}!")
+            else:
+                return number
+        except ValueError:
+            print(f"Value Error; enter a valid number that's >{MINIMUM_VALUE}!")
+
 
 main()
