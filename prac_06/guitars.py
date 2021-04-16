ca9c72821de6caac4600b9bc7083d9d29b3a3436
zebdelrosario <delrosariozeb@gmail.com>
2021-04-16 16:49:52 +1000

    Add get_valid_string function

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -6,6 +6,20 @@ MINIMUM_VALUE = 0
 def main():
     """Store all of input guitars in a list, then print their details."""
     print("My guitars!")
+    guitar_name = get_valid_string("Name: ")
+    guitar_year = get_valid_number("Year: ")
+    guitar_cost = float(get_valid_number("Cost: "))
+
+
+def get_valid_string(prompt):
+    """Prompt user until a valid string is entered."""
+    is_valid_string = False
+    while not is_valid_string:
+        try:
+            string = str(input(prompt))
+            return string
+        except ValueError:
+            print("Value Error, please enter a STRING!")
 
 
 def get_valid_number(prompt):
