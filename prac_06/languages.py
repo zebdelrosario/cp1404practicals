58edf66361f37a5ed5a280ad85b2101e1348712c
zebdelrosario <delrosariozeb@gmail.com>
2021-04-16 15:49:34 +1000

    Add docstring to main function

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -3,6 +3,7 @@ from programming_language import ProgrammingLanguage
 
 
 def main():
+    """Display a list of programming languages that are dynamically typed."""
     ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
     python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
     visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
