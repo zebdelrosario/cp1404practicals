ed9505be0b006be2b6473b84641fced29155623e
zebdelrosario <delrosariozeb@gmail.com>
2021-04-16 15:49:13 +1000

    Add docstring to class, fix PEP8 issue

---
+++ prac_06/guitar.py
@@ -0,0 +1,9 @@
+"""CP1404 Practical 06: Classes - Do-from-scratch exercises."""
+
+
+class Guitar:
+    """Represent a Guitar object."""
+    def __init__(self, name="", year=0, cost=0):
+        self.name = name
+        self.year = year
+        self.cost = cost
