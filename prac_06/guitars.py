6eb829ee6be54b7c958f6607074df78561080b33
zebdelrosario <delrosariozeb@gmail.com>
2021-04-16 16:39:58 +1000

    Initial commit

---
+++ prac_06/guitars.py
@@ -0,0 +1,12 @@
+"""CP1404 Practical 06: Classes - Do-from-scratch exercises."""
+from guitar import Guitar
+MINIMUM_VALUE = 0
+
+
+def main():
+    """Store all of input guitars in a list, then print their details."""
+    print("My guitars!")
+
+
+
+main()
