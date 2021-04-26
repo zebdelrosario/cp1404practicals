dc002f97f8a60e9051f0a26ba6a416851c23f353
zebdelrosario <delrosariozeb@gmail.com>
2021-04-26 15:55:16 +1000

    Add handle increment function

--- prac_07/convert_miles_km.py
+++ prac_07/convert_miles_km.py
@@ -2,7 +2,7 @@
 Kivy GUI program to convert miles to kilometres.
 Zeb del Rosario, Bachelor of IT
 Started 26/04/2021"""
-from kivy.app import App
+from kivy.app import App, StringProperty
 from kivy.lang import Builder
 from kivy.core.window import Window
 
@@ -11,6 +11,8 @@ __author__ = "Zeb del Rosario"
 
 class ConvertMilesToKmApp(App):
     """ConvertMilesToKmApp is a Kivy app that converts miles to km."""
+    message = StringProperty()
+
     def build(self):
         """Build the app using Kivy."""
         Window.size = (500, 350)
@@ -21,8 +23,13 @@ class ConvertMilesToKmApp(App):
     def handle_calculation(self, value):
         """Convert miles to kilometres, output result to """
         value *= 1.609323529411765  # convert mile to km
-        self.root.ids.output_label.text = f"{value:.3f}"
-        print(f"{value:.3f}")
+        self.message = self.root.ids.output_label.text = f"{value:.3f}"
+
+    def handle_increment(self, increment_value):
+        """Increase/decrease current entry by 1."""
+        input_value = int(self.root.ids.input_number.text)
+        input_value = input_value + int(increment_value)
+        self.root.ids.input_number.text = str(input_value)
 
 
 ConvertMilesToKmApp().run()
