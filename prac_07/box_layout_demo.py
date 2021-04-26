22e5dad666b50848215b8e9873ce125b1e041153
zebdelrosario <delrosariozeb@gmail.com>
2021-04-26 14:20:37 +1000

    Change Button to TextInput, Add output label to box_layout program

--- prac_07/box_layout_demo.py
+++ prac_07/box_layout_demo.py
@@ -8,5 +8,9 @@ class BoxLayoutDemo(App):
         self.root = Builder.load_file('box_layout.kv')
         return self.root
 
+    def handle_greet(self):
+        print("test")
+        self.root.ids.output_label.text = "Hello " + self.root.ids.input_name.text
+
 
 BoxLayoutDemo().run()
