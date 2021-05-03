ca6f71cf7c443bb6e8d624fc37767518ebf42e54
zebdelrosario <delrosariozeb@gmail.com>
2021-05-03 20:09:26 +1000

    Initial commit

---
+++ prac_08/taxi_simulator.py
@@ -0,0 +1,8 @@
+"""Practical 08 - Taxi Simulator program.
+Choose a taxi from a provided list and receive a bill corresponding to distance driven."""
+from prac_08.taxi import Taxi
+from prac_08.silver_service_taxi import SilverServiceTaxi
+
+
+def main():
+    """Taxi simulator: choose from a list of Taxis and drive."""
