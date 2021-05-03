a3143cd7754ae9ba995b6da04697912e9dff7be7
zebdelrosario <delrosariozeb@gmail.com>
2021-05-03 15:18:45 +1000

    Initial commit

---
+++ prac_08/car.py
@@ -0,0 +1,36 @@
+"""CP1404/CP5632 Practical - Car class example."""
+
+
+class Car:
+    """Represent a Car object."""
+
+    def __init__(self, name="", fuel=0):
+        """Initialise a Car instance.
+        name: type of vehicle (e.g.: limo, truck etc.)
+        fuel: float, one unit of fuel drives one kilometre
+        odometer: current kilometers travelled
+        """
+        self.name = name
+        self.fuel = fuel
+        self.odometer = 0
+
+    def __str__(self):
+        return f"name={self.name}, fuel={self.fuel}, odometer={self.odometer}"
+
+    def add_fuel(self, amount):
+        """Add amount to the car's fuel."""
+        self.fuel += amount
+
+    def drive(self, distance):
+        """Drive the car a given distance.
+
+        Drive given distance if car has enough fuel
+        or drive until fuel runs out return the distance actually driven.
+        """
+        if distance > self.fuel:
+            distance = self.fuel
+            self.fuel = 0
+        else:
+            self.fuel -= distance
+        self.odometer += distance
+        return distance
