1674fa9c6251e64c913d3c4dc51ec2c17910812f
zebdelrosario <delrosariozeb@gmail.com>
2021-05-03 16:34:16 +1000

    Add SilverServiceTaxi testing program

---
+++ prac_08/silver_service_taxi_test.py
@@ -0,0 +1,14 @@
+from prac_08.silver_service_taxi import SilverServiceTaxi
+
+
+def main():
+    print(f"""Silver Service Taxis!""")
+    taxi_one = SilverServiceTaxi("Taxi 1", 100, 2)
+    taxi_one.start_fare()
+    print("Starting Fare!\n")
+    taxi_one.drive(18)
+    print(f"Fare distance: {taxi_one.current_fare_distance:>8} km")
+    print(f"Current fare cost: $ {taxi_one.get_fare()}")
+
+
+main()
