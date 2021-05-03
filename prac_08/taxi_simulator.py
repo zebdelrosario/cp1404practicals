9b13710cf1094bfb897a3ef4c64b990011a29420
zebdelrosario <delrosariozeb@gmail.com>
2021-05-03 20:15:05 +1000

    Add get_valid_choice function

--- prac_08/taxi_simulator.py
+++ prac_08/taxi_simulator.py
@@ -2,7 +2,19 @@
 Choose a taxi from a provided list and receive a bill corresponding to distance driven."""
 from prac_08.taxi import Taxi
 from prac_08.silver_service_taxi import SilverServiceTaxi
+MENU_CHOICES = ["q", "c", "d"]
 
 
 def main():
     """Taxi simulator: choose from a list of Taxis and drive."""
+    menu_choice = get_valid_choice(">>>")
+
+
+def get_valid_choice(prompt):
+    """Prompt user until valid choice is selected."""
+    user_choice = input(prompt).lower()
+    while not user_choice in MENU_CHOICES:
+        print("Please select a valid menu option!")
+    return user_choice
+
+main()
