d0f334052cdf8d5cea8fab9173d9359154bf8933
zebdelrosario <delrosariozeb@gmail.com>
2021-05-03 15:32:45 +1000

    Print taxi_test's test run details

---
+++ prac_08/taxi_test.py
@@ -0,0 +1,18 @@
+from prac_08.car import Car
+from prac_08.taxi import Taxi
+
+# Print the taxi's details and the current fare
+new_taxi = Taxi("Prius 1", 100, 1.23)
+new_taxi.drive(40)
+print(f"""Taxi's details:
+{"Name:":>10} {new_taxi.name}
+Fare cost: $ {new_taxi.get_fare():>6.2f}\n""")
+
+
+# Restart the meter (start a new fare) and then drive the car 100km
+# Print the details and the current fare
+new_taxi.start_fare()
+new_taxi.drive(100)
+print(f"""Taxi's details:
+{"Name:":>10} {new_taxi.name}
+Fare cost: $ {new_taxi.get_fare():>6.2f}""")
