30d50a21a6684e76e44bfb38fb6b73640c858868
zebdelrosario <delrosariozeb@gmail.com>
2021-05-13 23:20:48 +1000

    Complete create_new_directory function, start move_files_to_directory function

--- prac_09/FilesToSort/sort_files_1.py
+++ prac_09/FilesToSort/sort_files_1.py
@@ -14,7 +14,22 @@ def main():
     print("-" * 50, f"\nFiles inside {ROOT_DIRECTORY}:")
     file_paths = determine_file_paths(ROOT_DIRECTORY)
     file_extensions = determine_file_extensions(file_paths)
+    for file_extension in file_extensions:
+        create_new_directory(ROOT_DIRECTORY, file_extension)
     print(file_extensions)
+
+
+def move_files_to_directory(root_directory, destination_directory):
+    """Move files from given root to destination directory."""
+
+
+def create_new_directory(root_directory, file_extension):
+    """Create a new directory named after given argument inside given file path."""
+    try:
+        os.mkdir(root_directory + "\\" + file_extension)
+    except FileExistsError:
+        print(f"Directory already exists; ignoring create command..")
+        pass
 
 
 def determine_file_paths(root_directory):
@@ -25,7 +40,7 @@ def determine_file_paths(root_directory):
 
 
 def determine_file_extensions(file_paths):
-    """Determine what file extensions are inside current folder."""
+    """Split file extensions from files inside given file paths."""
     roots_extensions = [os.path.splitext(file_path) for file_path in file_paths]  # split file path from ext
     file_extensions = [file_extension for root, file_extension in roots_extensions]  # store ext in list
     for file_extension in file_extensions:  # remove multiples of extension
