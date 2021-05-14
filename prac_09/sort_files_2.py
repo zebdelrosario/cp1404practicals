586a0619b9f7f97d53c83b66bd9ad16ed35948a0
zebdelrosario <delrosariozeb@gmail.com>
2021-05-14 18:37:49 +1000

    Complete move_files_to_directory function, refactor determine_file_extensions function variables

--- prac_09/sort_files_2.py
+++ prac_09/sort_files_2.py
@@ -5,21 +5,34 @@ Start date: 13/05/2021
 """
 
 import os
+import shutil
 
 ROOT_DIRECTORY = os.getcwd() + "\\" + "FilesToSort"  # force cwd to be set to "FilesToSort"
 
 
 def main():
     """Sort files v2; sort files based on user input."""
+    categories_extensions_dic = {}
     print(f"Root directory: {ROOT_DIRECTORY}")
     print("-" * 50)
     source_file_paths = determine_file_paths(ROOT_DIRECTORY)
     file_extensions = determine_file_extensions(source_file_paths)
-    for file_extension in file_extensions:
-        destination_directory_name = input(f"What category would you like to sort {file_extension} files into? ")
-        destination_directory_path = ROOT_DIRECTORY + "\\" + destination_directory_name
-        create_new_directory(ROOT_DIRECTORY, destination_directory_name)
-        print(destination_directory_path)
+    print(source_file_paths, "\n")
+    for index, file_extension in enumerate(file_extensions):
+        file_category_name = input(f"What category would you like to sort {file_extension} files into? ")
+        destination_directory_path = ROOT_DIRECTORY + "\\" + file_category_name
+        create_new_directory(ROOT_DIRECTORY, file_category_name)
+        for source_file_path in source_file_paths:
+            if source_file_path.endswith(file_extension):
+                move_files_to_directory(source_file_path, destination_directory_path)
+
+
+def move_files_to_directory(source_file_path, destination_directory_path):
+    """Move files from given root to destination directory."""
+    try:
+        shutil.move(source_file_path, destination_directory_path)
+    except FileExistsError:
+        pass
 
 
 def create_new_directory(root_directory, new_directory_name):
@@ -40,11 +53,12 @@ def determine_file_paths(root_directory):
 
 def determine_file_extensions(file_paths):
     """Split file extensions from files inside given file paths."""
+    file_extensions = []
     roots_extensions = [os.path.splitext(file_path) for file_path in file_paths]  # split file path from ext
-    file_extensions = [file_extension.strip(".") for root, file_extension in roots_extensions]  # store ext in list
-    for file_extension in file_extensions:  # remove multiples of extension
-        if file_extensions.count(file_extension) > 1:
-            file_extensions.remove(file_extension)
+    raw_file_extensions = [file_extension.strip(".") for root, file_extension in roots_extensions]  # store ext in list
+    for raw_file_extension in raw_file_extensions:
+        if raw_file_extension not in file_extensions:
+            file_extensions.append(raw_file_extension)
     return file_extensions
 
 
