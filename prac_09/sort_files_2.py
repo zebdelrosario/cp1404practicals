971f5d29ca17534c4c334a9c93f49231bce4cafb
zebdelrosario <delrosariozeb@gmail.com>
2021-05-14 15:49:06 +1000

    Add create_new_directory function

--- prac_09/sort_files_2.py
+++ prac_09/sort_files_2.py
@@ -16,7 +16,19 @@ def main():
     source_file_paths = determine_file_paths(ROOT_DIRECTORY)
     file_extensions = determine_file_extensions(source_file_paths)
     for file_extension in file_extensions:
-        destination_folder_name = input(f"What category would you like to sort {file_extension} files into? ")
+        destination_directory_name = input(f"What category would you like to sort {file_extension} files into? ")
+        destination_directory_path = ROOT_DIRECTORY + "\\" + destination_directory_name
+        create_new_directory(ROOT_DIRECTORY, destination_directory_name)
+        print(destination_directory_path)
+
+
+def create_new_directory(root_directory, new_directory_name):
+    """Create a new directory named after given argument inside given file path."""
+    try:
+        os.mkdir(root_directory + "\\" + new_directory_name)
+    except FileExistsError:
+        print(f"Directory already exists; ignoring create command..")
+        pass
 
 
 def determine_file_paths(root_directory):
@@ -29,7 +41,7 @@ def determine_file_paths(root_directory):
 def determine_file_extensions(file_paths):
     """Split file extensions from files inside given file paths."""
     roots_extensions = [os.path.splitext(file_path) for file_path in file_paths]  # split file path from ext
-    file_extensions = [file_extension for root, file_extension in roots_extensions]  # store ext in list
+    file_extensions = [file_extension.strip(".") for root, file_extension in roots_extensions]  # store ext in list
     for file_extension in file_extensions:  # remove multiples of extension
         if file_extensions.count(file_extension) > 1:
             file_extensions.remove(file_extension)
